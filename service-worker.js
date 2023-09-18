7dcd5963ac0dafb36966545cab0767ee7b75e321
Premsai Varma <premsaivarmacoding@gmail.com>
2023-09-18 17:07:29 +0530

    updated service-worker for webapp

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,40 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-cache-name').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/assets/css/main.css',
-          '/assets/js/main.js',
-          '/assets/img/icon.png',
-          '/teamMembers.html/'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
-  
+const CACHE_NAME = 'ieee-cs-web-app-v1'; // Update with your preferred cache name
+const CACHE_FILES = [
+  '/',
+  '/404.html',
+  '/index.html',
+  '/assets/css/main.css', // Update the paths for CSS, JS, and other assets
+  '/assets/js/main.js',
+  '/assets/img/logo.png',
+  // Add other paths to static assets you want to cache
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
